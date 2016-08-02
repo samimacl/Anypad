fdb020b2daa5bbe59baebd7a13e407eb4bf3ccdd
felixalter <felix.guendling@ba-rm.de>
2016-08-02 11:21:43 +0200

    update 02.08 11:00.

--- src/editor/storage/storage.js
+++ src/editor/storage/storage.js
@@ -2,28 +2,31 @@
 // die datei in der die zeilen gespeichert werden.
 var datei = null;
 
-
-//soll inhalte aus html als .json file sichern.
+// soll inhalte aus html als .json file sichern. Tut dies inklusive Tags. 
+// Die resultierende Datei hat die Dateieindung .json und enthält ein json-Objekt 
+// mit dem name-value-pair "content":"inhalt der seite".
 function exportJSON () {
 	 
-
-	 //muss angepasst werden, damit die datei tatsächlich wie ein json objekt aufgebaut ist.
 	 datei = document.getElementById("text").innerHTML;
-	 var dataStr = "data:text/json;charset=utf-8," + datei;//encodeURIComponent(JSON.stringify(data));
+	 //neu:
+	 var dataStr = "data:text/json;charset=utf-8," + 
+	 "data = [{\"content\"" + ": " + "\"" + datei + "\"}];";//encodeURIComponent(JSON.stringify(data));
+
+	 //download der datei
 	 var dlAnchorElem = document.getElementById('downloadAnchorElement');
 	 dlAnchorElem.setAttribute("href",     dataStr     );
 	 dlAnchorElem.setAttribute("download", "backup.json");
 	 dlAnchorElem.click();
 
 };
-
+//Eine Hilfsfunktion zum Testen, die den text einer seite löscht.
 function resetText(){
 	var leer = "";
 	document.getElementById("text").innerHTML = leer;
 };
 
 
-
+//war gedacht um die quelle des inputs zu ändern:
 /*function modifySource(){
 
 	var input = prompt("Please enter the directory where the data is stored:", "filename.json");
@@ -33,13 +36,14 @@ function resetText(){
 
 function load(){
 
-// die "quelle" (data) ist statisch im html hinterlegt und müsste dynamisch festgelegt werden.
-// es muss dann das ganze array durchgegangen werden um alle inhalte zu laden.
+// die "quelle" people.json, bzw. "data" (ist der name der variable innerhalb von people.json)
+// ist statisch im html hinterlegt und müsste dynamisch festgelegt werden.
 
 //var mydata = JSON.parse(data);
 //modifySource();
+
 var mydata = (data);
-document.getElementById("text").innerHTML = mydata[0].name + " " + mydata[0].age;
+document.getElementById("text").innerHTML = mydata[0].content;
 
 };
 
@@ -50,6 +54,7 @@ document.getElementById("text").innerHTML = mydata[0].name + " " + mydata[0].age
 
 
 
+// erste Versuche, wird nicht weiter benötigt.
 var beispiel = '{ "beispiel" : [' +
 '{ "id":"p1" ,"opening":"<P>", "inhalt":"das hier ist p1", "closing":"</p>" },' +
 '{ "id":"p2" , "content":"das hier ist p2" }]}';
@@ -64,9 +69,3 @@ function loadJSON(string){
 	//evtl muss ein string entgegengenommen werden, der dann wieder zu einem json objekt gemacht
 	//werden muss
 };
-
-
-
-
-
-
