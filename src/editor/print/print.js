d43816343767156cb83cd91a3b22fbdceb87107c
GitHAndy <andreas.garben@ba-rm.de>
2016-08-05 15:43:02 +0200

    Dokumentation hinzugefügt

--- src/editor/print/print.js
+++ src/editor/print/print.js
@@ -1,10 +1,20 @@
 var util = util || {};
 
+/**
+ * Erstellung eines Printobjekt
+ * Deklarierung einr Funktion zum Weierverabeiten des HTLM aus dem iFrame
+ * @class
+ * @constructor
+ * @param {window} printwindow - Neues Fenster bzw. Druckvorschau
+ * @param {string} innerHTML - Übergebener HTML-Element zum Drucken
+ */
+
 util.print = util.print || function () {
   this.doPrint = function ( innerHTML ) {
     var printwindow = window.open();
     try {
       printwindow.document.write( innerHTML );
+      printwindow.document.close();
       printwindow.print();
     }
     catch(err) {
